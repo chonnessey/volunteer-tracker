dd451ba0dd72989334dcb286ca361cc447c97c3f
Adrian Camacho <adriancamacho18@gmail.com>
2021-07-30 14:04:25 -0700

    add rinitial routes and first passing integration test

--- app.rb
+++ app.rb
@@ -5,3 +5,21 @@ require('./lib/volunteer')
 require('pry')
 also_reload('lib/**/*.rb')
 require("pg")
+
+DB = PG.connect({ dbname: 'volunteer_tracker'})
+
+get('/') do
+  redirect to('/projects')
+end
+
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+post('/projects') do
+  title = params[:title]
+  project = Project.new({:title=> title, :id => nil})
+  project.save()
+  redirect to('/projects')
+end
