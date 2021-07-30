90568b82073c420a5a29e6973cbf2e565e8e4040
Adrian Camacho <adriancamacho18@gmail.com>
2021-07-30 11:07:34 -0700

    add passing test for function method

--- lib/project.rb
+++ lib/project.rb
@@ -34,4 +34,12 @@ class Project
     id = project.fetch("id").to_i
     Project.new({:title => title, :id => id})
   end
+
+  def update(attributes)
+    if (attributes.has_key?(:title)) && (attributes.fetch(:title) != nil)
+      @title = attributes.fetch(:title)
+      DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
+    end
+  end
+
 end
