e8410fb89ce99f5d19505b9de1e96b53681afb95
Adrian Camacho <adriancamacho18@gmail.com>
2021-08-08 21:08:19 -0700

    fix bug in app.rb to now be able to update a project title

--- app.rb
+++ app.rb
@@ -40,7 +40,7 @@ end
 
 patch('/projects/:id') do
   @project = Project.find(params[:id].to_i())
-  @project.update(params[:title])
+  @project.update({:title => params[:title]})
   redirect to('/projects')
 end
 
